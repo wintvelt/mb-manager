5aa85ba7c36dea974c67d31f1a1877d5b3a5c581
wintvelt <wintvelt@me.com>
2019-06-10 19:33:17 +0200

    New Home screen, moved playground to secret page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,75 +1,61 @@
 //Content on Home page
-import React from 'react';
-import { connect } from "react-redux";
-import { login, logout, doSnack } from "../actions/actions";
-import { batchLedgerUpdate } from "../actions/batchActions";
-import { getContacts } from '../actions/apiActions';
-import { FileZone } from '../constants/file-helpers';
+import React, { Component } from 'react';
 
-function mapDispatchToProps(dispatch) {
-  return {
-    login: () => dispatch(login()),
-    logout: () => dispatch(logout()),
-    doSnack: (newSnack) => dispatch(doSnack(newSnack)),
-    batchLedgerUpdate: (list) => dispatch(batchLedgerUpdate(list)),
-    getContacts: () => dispatch(getContacts())
-  };
-}
+const cards =
+	[
+		{
+			title: "Welkom",
+			par: "Welkom bij Moblybird, een online hulp voor Moneybird voor Mobly. Via deze site kun je bepaalde dingen makkelijker en sneller doen dan direct op de Moneybird site. Het is iets makkelijker om bepaalde facturen, betalingen, of contacten terug te vinden. Daarnaast kun je met Moblybird ook meerdere bewerkingen in 1 keer doen, zoals bijvoorbeeld een set van facturen aan een categorie (rekening) toewijzen."		
+		},
+		{
+			title: "Connectie",
+			par: "Handig om bij de connectie met Moneybird te starten. Zie de link in het menu rechtsboven. Als het poppetje wit is, dan moet je nog een connectie maken. Klik op dat menu item, en volg de instructies daar."
+		}
+	].map((obj, i) => { return Object.assign({}, obj, { id: i }) });
 
-function mapStateToProps(state) {
-  return { 
-  	counter : state.counter,
-  	contacts : state.contacts
-  };
-}
-
-const buttons = ["Test2", "Test3", "Test4", ""];
-
-const testBatch = [
-	{ incomingId: "243603922888950994", newLedgerName: "Algemene kosten"},
-	{ incomingId: "243603935219156242", newLedgerName: "Algemene kosten"},
-	{ incomingId: "243687580448589544", newLedgerName: "Algemene kosten"}
-];
-
-const ConnectedHome = (props) => {
+const cardView = (card, clickFunc) => {
 	return (
-	    <div className="container">
-			<div className="row">
-				<h5>Inhoud enzo</h5>
+		<div className="col s12 m4" key={card.id}>
+			<div className="card blue-grey darken-1">
+				<div className="card-content white-text">
+					<span className="card-title">{card.title}</span>
+					<p>{card.par}</p>
+				</div>
+				<div className="card-action">
+					<button className="btn-flat orange-text" onClick={e => clickFunc(card.id)}>Got it!</button>
+				</div>
 			</div>
-	      	<div className="row">
-		    	<h5>Test snackbar</h5>
-			    {buttons.map( (snack, index) => {
-				    	return <span className="btn waves-effect waves-light" 
-				    		style={{marginRight: "4px"}}
-				    		key={snack} 
-				    		onClick={() => props.doSnack(snack)}>{snack}</span>
-				    })}
-		  	</div>
-		    <div className="row">
-		    	<h5>Test Login</h5>
-		    	<span className="btn" onClick={props.login} style={{marginRight: "4px"}}>Login</span>
-		    	<span className="btn" onClick={props.logout}>Logout</span>
-			</div>
-			<div className="divider"></div>
-			<div className="row">
-				<h6>Voor testing..</h6>
-				<span className="btn" onClick={() => props.batchLedgerUpdate(testBatch)}>Test Batch</span>
-				<span className="btn" onClick={() => props.getContacts()}>Get contacts</span>
-				<p>{(props.contacts)?props.contacts.length:0} contacten bekend</p>
-			</div>
-			<div className="divider"></div>
-			<div className="row">
-				<h6>File Load testing..</h6>
-				<p><span className="btn" onClick={() => console.log('click!!')}>Get File</span></p>
-				<FileZone />
-			</div>
-
 		</div>
 	);
 }
 
-const Home = connect(mapStateToProps,mapDispatchToProps)(ConnectedHome);
+class Home extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { cards: cards }
+		this.dismiss = this.dismiss.bind(this)
+	}
+	dismiss(id) {
+		this.setState({ cards: this.state.cards.filter(c => (c.id !== id)) });
+	}
+	render() {
+		return (
+			<div className="container">
+				<div className="row">
+					{this.state.cards.map(c => cardView(c, this.dismiss))}
+				</div>
+				<div className="row">
+					<h5>I am h5 header</h5>
+				</div>
+				<div className="divider"></div>
+				<div className="row">
+					<h6>I am H6 header</h6>
+				</div>
 
+			</div>
+		);
+	}
+
+}
 
 export default Home;
