f5b0e327f8bea39f1399ed89ad80724c609d7128
wintvelt <wintvelt@me.com>
2019-08-29 10:06:29 +0200

    Adding check if file was sent before

--- src/components/Bankmutations-ActiveAccount.js
+++ src/components/Bankmutations-ActiveAccount.js
@@ -12,7 +12,8 @@ import { BankActiveCsv } from './Bankmutations-ActiveCsv';
 export const ActiveAccount = (props) => {
     const { accessToken } = useSelector(store => store);
     const { bankData, admin } = props;
-    const [isOpen, setisOpen] = useState(false);
+    const [adminIsOpen, setadminIsOpen] = useState(false);
+    const [askConfirm, setAskConfirm] = useState({ ask: false });
     const dispatch = useDispatch();
     const fileHandler = (files) => {
         let errorMsg = '';
@@ -26,7 +27,7 @@ export const ActiveAccount = (props) => {
             let reader = new FileReader();
             reader.onload = (e) => {
                 dispatch(setBank({ type: 'setCsvWithOrigin', content: { data: e.target.result, filename: file.name } }));
-                convertCsvData(file.name, e.target.result)
+                maybeConvertCsvData(file.name, e.target.result)
             };
             reader.readAsText(file);
         }
@@ -40,9 +41,20 @@ export const ActiveAccount = (props) => {
             accessToken,
             loadingMsg: 'Even geduld terwijl we csv bestand ophalen',
             dispatch,
-            callback: (data) => convertCsvData(filename, data)
+            callback: (data) => maybeConvertCsvData(filename, data)
         }
         fetchAWSAPI(getCsvOptions);
+    }
+    const maybeConvertCsvData = (filename, data) => {
+        console.log('checking maybe');
+        // check if filename is already converted
+        if (fileInList(filename, bankData.files)) {
+            console.log('should ask');
+            setAskConfirm({ ask: true, filename, data});
+        } else {
+            console.log('converting');
+            convertCsvData(filename, data);
+        }
     }
     const convertCsvData = (filename, data) => {
         const postBody = {
@@ -50,7 +62,6 @@ export const ActiveAccount = (props) => {
             csv_content: data,
             convert_only: false
         }
-        const loadingMsg = `bezig met converteren ${filename}`;
         const getFilesOptions = {
             stuff: bankData.files,
             path: '/files/' + bankData.activeAccount.value,
@@ -60,6 +71,7 @@ export const ActiveAccount = (props) => {
             loadingMsg: 'Even geduld terwijl we folderinhoud ophalen',
             dispatch
         }
+        const loadingMsg = `bezig met converteren ${filename}`;
         const convertCsvOptions = {
             method: 'POST',
             body: postBody,
@@ -75,7 +87,16 @@ export const ActiveAccount = (props) => {
         fetchAWSAPI(convertCsvOptions);
     }
     const onClickAdmin = () => {
-        setisOpen(!isOpen);
+        setadminIsOpen(!adminIsOpen);
+    }
+    const onConfirmConvert = (ok) => {
+        if (ok) {
+            console.log({askConfirm});
+            convertCsvData(askConfirm.filename, askConfirm.data);
+        } else {
+            dispatch(setBank({ type: 'setCsv', content: { INIT: true } }));
+            setAskConfirm({ask:false});
+        }
     }
     return (
         <div>
@@ -87,10 +108,14 @@ export const ActiveAccount = (props) => {
             }
             <BankActiveCsv activeCsv={bankData.activeCsv} />
             {(bankData.config.hasAllData && bankData.convertResult.hasAllData && bankData.convertResult.data &&
-                (bankData.convertResult.data.errors || isOpen)) ?
+                (bankData.convertResult.data.errors || adminIsOpen)) ?
                 <BankConfig account={bankData.activeAccount.value}
                     config={bankData.config.data} convertResult={bankData.convertResult.data}
                     files={bankData.files} />
+                : <></>
+            }
+            {(askConfirm.ask)?
+                <Confirmation onClick={onConfirmConvert} filename={askConfirm.filename} />
                 : <></>
             }
             {(!bankData.files.hasAllData && !bankData.files.hasError) ?
@@ -104,7 +129,7 @@ export const ActiveAccount = (props) => {
             <pre>(config){JSON.stringify(bankData.config, null, 2)}</pre>
             <pre>(convertResult){JSON.stringify(bankData.convertResult, null, 2)}</pre>
             <pre>(files){JSON.stringify(bankData.files, null, 2)}</pre>
-            {(admin) ? <AdminButton isOpen={isOpen} onClick={onClickAdmin}
+            {(admin) ? <AdminButton adminIsOpen={adminIsOpen} onClick={onClickAdmin}
                 enabled={(bankData.config.hasAllData && bankData.convertResult.hasAllData)} />
                 : <></>
             }
@@ -125,11 +150,50 @@ const Loader = ({ apiData, className }) => {
 }
 
 const AdminButton = (props) => {
-    const { onClick, isOpen, enabled } = props;
+    const { onClick, adminIsOpen, enabled } = props;
     const btnClassBasic = 'btn btn-floating btn-small orange admin';
     const btnClass = (enabled) ? btnClassBasic : btnClassBasic + ' disabled';
-    const icon = (isOpen) ? 'close' : 'settings';
+    const icon = (adminIsOpen) ? 'close' : 'settings';
     return <button className={btnClass} onClick={onClick}>
         <i className='material-icons'>{icon}</i>
     </button>
+}
+
+const fileInList = (filename, files) => {
+    const shortFilename = filename.split('.')[0];
+    console.log({files});
+    return (files.data && files.data.length > 0 &&
+        files.data.filter(f => {
+            const fileFromPath = f.filename.split('/').slice(1)[0];
+            console.log({fileFromPath, shortFilename});
+            return (fileFromPath === shortFilename && f.send_result_ok)
+        }).length > 0)
+}
+
+const Confirmation = ({ filename, onClick }) => {
+    const onConfirm = () => onClick(true);
+    const onCancel = () => onClick(false);
+
+    return (
+        <div className="row">
+            <div className="col s12 m6 offset-m3">
+                <div className="card center">
+                    <span className="card-title">Vraagje</span>
+                    <div className="card-content">
+                        <p>Het bestand {filename} heb je al eerder naar Moneybird doorgestuurd.
+                        Weet je zeker dat je dit opnieuw wilt doen? 
+                        Had je het bijbehorende bestand uit Moneybird al verwijderd?</p>
+                    </div>
+                    <div className="card-action right-align">
+                        <span className="btn-flat hide-on-large-only red-text text-lighten-3" onClick={onCancel}>
+                            Annuleer</span>
+                        <span className="btn-flat hide-on-large-only teal-text" onClick={onConfirm}>
+                            OK</span>
+                        <span className="btn-flat hide-on-med-and-down red-text text-lighten-3" onClick={onCancel}>
+                            Nee, Annuleren</span>
+                        <span className="btn-flat hide-on-med-and-down teal-text" onClick={onConfirm}>Ja, Verwerk</span>
+                    </div>
+                </div>
+            </div>
+        </div>)
 }
