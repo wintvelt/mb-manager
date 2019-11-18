c3137f7116c50cf0a8edb1f95b7ea1b591cda63a
wintvelt <wintvelt@me.com>
2019-11-18 12:20:33 +0100

    fixed access verified status

--- src/store/derived-storestate-helpers.js
+++ src/store/derived-storestate-helpers.js
@@ -2,13 +2,16 @@
 
 export const hasData = (store) => {
     const apiDataKeys = [
-        {key: 'payments', isMulti: true},
-        {key: 'contactsNew', isMulti: true},
-        {key: 'accountsNew'}
+        { key: 'payments', isMulti: true },
+        { key: 'contactsNew', isMulti: true },
+        { key: 'accountsNew' },
+        { key: 'receipts', isMulti: true },
+        { key: 'purchaseInvoices', isMulti: true },
+        { key: 'ledgersNew' },
     ];
     return apiDataKeys.reduce((res, entry) => {
-        const hasData = entry.isMulti? 
+        const hasData = entry.isMulti ?
             store[entry.key].get('apiData').toJS().hasData : store[entry.key].toJS().hasData
         return res || hasData;
-    },false)
+    }, false)
 }
