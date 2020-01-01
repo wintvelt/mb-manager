0851bed87d1348947deca378e09721f1a924d13d
wintvelt <wintvelt@me.com>
2020-01-01 14:38:57 +0100

    moved bookings more button to summary

--- src/components/Revenues/RevenuesData.js
+++ src/components/Revenues/RevenuesData.js
@@ -39,7 +39,8 @@ export default (props) => {
         dispatch(getPayments(access_token, periodOptions[period].value, ',state:unprocessed'));
     }, [dispatch, access_token, period]);
 
-    const handleMore = () => {
+    const handleMore = (e) => {
+        e.stopPropagation();
         setPeriod(period + 1);
     }
 
@@ -54,13 +55,14 @@ export default (props) => {
             'bankrekeningen',
             'boekingsregels'
         ]}
-        loadingText={loadingApiText} >
-        <Button color='primary' className={classes.listButton}
+        loadingText={loadingApiText} 
+        actionsInSummary>
+        <Button color='primary' className={classes.listButton} variant='contained'
             disabled={(nextPeriod) ? false : true}
             onClick={handleMore}>
             {(nextPeriod) ?
-                `Betalingen ${nextPeriod} ophalen..`
-                : `Alle betalingen (${curPeriod}) zijn opgehaald`}
+                `meer ophalen: ${nextPeriod}`
+                : `Alles (${curPeriod}) opgehaald`}
         </Button>
     </DataPanel >
 }
