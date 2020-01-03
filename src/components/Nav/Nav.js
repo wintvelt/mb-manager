26758d7ad59ecd311d25970847cb93521c2dd9fc
wintvelt <wintvelt@me.com>
2020-01-03 12:39:20 +0100

    changed order of Nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,9 +4,9 @@ import { NavWrapper } from './Nav-helpers';
 import { hasData } from '../../store/derived-storestate-helpers';
 
 const menu = [
-    { icon: 'import_contacts', link: '/contacten', text: 'Contacten' },
     { icon: 'cloud_upload', link: '/bankupload', text: 'Upload bankfile' },
     { icon: 'remove_shopping_cart', link: '/betalingen/uitgaven', text: 'Uitgaven zonder bon' },
+    { icon: 'import_contacts', link: '/contacten', text: 'Contacten' },
     { icon: 'receipt', link: '/inkomend', text: 'Bonnetjes en facturen' },
     { icon: 'attachment', link: '/betalingen/match', text: 'Betalingen matchen' },
     { icon: 'cake', link: '/revenues', text: 'Omzet verwerken', badge: true },
