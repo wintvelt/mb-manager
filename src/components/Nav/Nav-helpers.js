524da4e0ef33a983ada504a1275a8d55fb8544cc
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.local>
2020-03-11 10:55:14 +0100

    changed version name for hotfix

--- src/components/Nav/Nav-helpers.js
+++ src/components/Nav/Nav-helpers.js
@@ -23,7 +23,7 @@ import Link from '@material-ui/core/Link';
 import Tooltip from '@material-ui/core/Tooltip';
 
 const drawerWidth = 240;
-const version = 'versie 2.1.1';
+const version = 'versie 2.1.2';
 
 const useStyles = makeStyles(theme => ({
     root: {
