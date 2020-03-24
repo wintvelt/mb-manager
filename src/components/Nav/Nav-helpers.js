facdd13e69e5733dfb301ea71440b2c38e4b8b17
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-03-24 22:51:08 +0100

    updates redir_url and version

--- src/components/Nav/Nav-helpers.js
+++ src/components/Nav/Nav-helpers.js
@@ -23,7 +23,7 @@ import Link from '@material-ui/core/Link';
 import Tooltip from '@material-ui/core/Tooltip';
 
 const drawerWidth = 240;
-const version = 'versie 2.2.1';
+const version = 'versie 2.2.2';
 
 const useStyles = makeStyles(theme => ({
     root: {
